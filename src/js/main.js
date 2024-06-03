f3f2407d73bb06276db5392e1d82bd76e8458d20
Ulich Igor <ylichigor@gmail.com>
2024-06-03 06:07:31 +0300

    card-body

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 "use strict";
 $(function () {
   $('.bestsellers').slick({
-
+    dots: false,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev bestsellers-prev"></button>',
+    nextArrow:'<button type="button" class="slick-next bestsellers-next">Next</button>',
     responsive: [
       {
         breakpoint: 1024,
@@ -13,7 +15,7 @@ $(function () {
           slidesToShow: 3,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: false,
         }
       },
       {
